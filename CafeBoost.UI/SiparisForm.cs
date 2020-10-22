841ca0bc6f7f6cff6f6a460f7e07952b187b70a0
AliOzbek <alimustafaozbek@gmail.com>
2020-10-22 15:28:09 +0300

    kod devam

--- CafeBoost.UI/SiparisForm.cs
+++ CafeBoost.UI/SiparisForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CafeBoost.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,22 @@ namespace CafeBoost.UI
 {
     public partial class SiparisForm : Form
     {
-        public SiparisForm()
+        private readonly KafeVeri db;
+        private readonly Siparis siparis;
+        public SiparisForm(KafeVeri kafeVeri, Siparis siparis)
         {
+            //Constructor parametresi olarak gelen bu nesneleri daha sonra da erişebileceğimiz fieldlara aktarıyoruz.
+            db = kafeVeri;
+            this.siparis = siparis;
             InitializeComponent();
+            MasaNoGuncelle();
+        }
+
+        private void MasaNoGuncelle()
+        {
+            Text = $"Masa {siparis.MasaNo:00} - Sipariş Detayları (Açılış: {siparis.AcilisZamani.Value.ToShortTimeString()})";
+            lblMasaNo.Text = siparis.MasaNo.ToString("00");
+
         }
     }
 }
